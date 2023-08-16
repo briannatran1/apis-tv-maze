944a72acd772cb131edeadc8c74cd37d23272baa
Brianna Tran <briannatran45@gmail.com>
2023-08-16 14:49:25 -0700

    completed getShowsByTerm

--- tvmaze.js
+++ tvmaze.js
@@ -13,31 +13,18 @@ const TVMAZE_URL = 'https://api.tvmaze.com'
  */
 
 async function getShowsByTerm(term) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
+  // Remove placeholder & make request to TVMaze search shows API.
   const params = new URLSearchParams({q: term});
   const response = await fetch(`${TVMAZE_URL}/search/shows?${params}`);
   const showData = await response.json();
+  console.log('showData = ', showData);
+
+  for(let i = 0; i < showData.length; i++){
+    const {id, name, summary, image} = showData[i].show;
+    // console.log({id, name, summary, image});
+  }
 
   return showData;
-
-  // return [
-  //   {
-  //     id: 1767,
-  //     name: "The Bletchley Circle",
-  //     summary:
-  //       `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-  //          women with extraordinary skills that helped to end World War II.</p>
-  //        <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-  //          normal lives, modestly setting aside the part they played in
-  //          producing crucial intelligence, which helped the Allies to victory
-  //          and shortened the war. When Susan discovers a hidden code behind an
-  //          unsolved murder she is met by skepticism from the police. She
-  //          quickly realises she can only begin to crack the murders and bring
-  //          the culprit to justice with her former friends.</p>`,
-  //     image:
-  //         "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-  //   }
-  // ]
 }
 
 
